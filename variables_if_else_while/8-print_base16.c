396100d36f2228a5fc7aa0ee868f43ca6cd9b5a3
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-17 16:30:58 +0200

    creating 8 task

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+	 *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 101; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

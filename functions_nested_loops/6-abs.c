1e1f50a29b868295c2af6c93c5792875d76f5b8c
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-18 21:00:34 +0200

    modif on 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,21 +1,14 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer
- * @int: int
- * Return: 0
+ * @i: int
+ * Return: i
  */
 int _abs(int i)
 {
-	int j;
-
 	if (i < 0)
 	{
-		j = i * (-1);
-		_putchar(j);
+		i = -i;
 	}
-	else
-	{
-		_putchar(i);
-	}
-	return (0);
+	return i;
 }

ecd9d79b9eb44b61686a60aed2a84b0b9f73e569
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-18 15:16:08 +0200

    creating script for task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+	 *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet() 
+{
+	char c;
+  	for (c = 'a'; c <= 'z'; c++)
+	{
+       		_putchar('c');
+	}
+	_putchar('\n');
+}

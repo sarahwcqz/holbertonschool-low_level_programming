3c49ec19a20a4587ba29859eb886934405003102
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-18 15:26:38 +0200

    modif betty 1 task

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
 	 *
  * Return: Always 0 (Success)
  */
-void print_alphabet() 
+void print_alphabet(void)
 {
 	char c;
-  	for (c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-       		_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }

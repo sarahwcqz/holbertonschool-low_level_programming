3218e19435f7339064d6326bb9c76b256d280a90
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-23 15:01:50 +0200

    modif on 9, suppr last space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,14 @@ int main(void)
 		}
 		else if ((x % 5) == 0)
 		{
-			printf("Buzz ");
+			if (x == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{

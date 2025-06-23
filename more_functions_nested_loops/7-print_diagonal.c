9e111dac16b8e0f364eacf8854ab3f05c44ffdd7
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-23 15:52:53 +0200

    modif on 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,11 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n; x++)
 		{
-			for (y = 0; y < x; y++)
+			for (y = 0; y <= x; y++)
 			{
 				if (x == y)
 				{
-					_putchar('\\');
+					_putchar(92);
 				}
 				else
 				{

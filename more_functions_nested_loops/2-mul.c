ecb806de64a8a771de7dc2d2e360c1f249b095f9
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-23 10:37:48 +0200

    task 2 multipl two int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two int
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * print_numbers - prints numbers from 0 to 9
+ * @n: int
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}

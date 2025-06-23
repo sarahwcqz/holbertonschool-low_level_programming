6d15f8e573102e0e06d5f958bc64f002ba70e42a
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-23 14:55:19 +0200

    task 9 print number, display diff if mult of 3 and 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (((x % 3) == 0) && ((x % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

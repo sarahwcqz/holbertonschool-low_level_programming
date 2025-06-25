89b042afd876de4bd00b212b939804c4be04dc16
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-25 13:01:32 +0200

    task 8 : printing n elements of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints the n elements of an array
+ * @a: pointer
+ * @n: int
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n - 1)
+	{
+		printf("%i, ", a[i]);
+		i++;
+	}
+	printf("%i", a[i]);
+	printf("\n");
+}

9c18e08b6cab171b7e75bed419cf74bd49159121
sarahwcqz <sarah.wacquiez@gmail.com>
2025-06-25 08:24:41 +0200

    task 6: print every other char of a str

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char of a str
+ * @str: pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}

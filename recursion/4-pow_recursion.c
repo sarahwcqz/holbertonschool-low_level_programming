dd5baf892a52e8859145f289f519aedabe0f67f0
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-01 13:34:30 +0200

    task 4 : x pow y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: one of the int
+ * @y: the int to be put in power
+ * Return: int, the result of x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}

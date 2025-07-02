4755a61b2ce2125f1e018a864f23d4f16c2d67a2
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-02 10:17:41 +0200

    task 1 proj argvargc : print the nmbr of arg passed to the prgm

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point, prgm that prints the numbr of args passed into it
+ * @argc: numbr of args
+ * @argv: array containing the args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		i++;
+	}
+	printf("%d\n", i);
+	return (0);
+}

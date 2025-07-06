7cfbebfaa1ae13a37eafc8e69ab94bdf132dfb0f
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-06 10:18:31 +0200

    task 2 proj more malloc, creating 2 scenari, one w/ for loop and int ptr, one w/ memset and void ptr (planB)

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates the mem of an arr in initialize it with 0
+ * @nmemb: number of elements
+ * @size: the size of the elements
+ * Return: pointer to the allocated mem, NULL if failed
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	int *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == 0)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
+/* version a la mano, mais oblige a un ptr de type int, pas tres coherent*/
---
+++ more_malloc_free/2-callocplanB.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - allocates the mem of an arr in initialize it with 0
+ * @nmemb: number of elements
+ * @size: the size of the elements
+ * Return: pointer to the allocated mem, NULL if failed
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == 0)
+		return (NULL);
+
+	memset(ptr, 0, (size * nmemb));
+
+	return (ptr);
+}
+/**
+ * version avec memset, permet d'avoir un ptr de type void,
+ * mais pas sure que ca passe le checkeur
+ */

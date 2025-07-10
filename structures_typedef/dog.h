41f4b30d81ef0d73b5dbf7f1127150e191d3ca8f
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-10 10:53:48 +0200

    modif on header proj struct/typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure to have info about a dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: the provider of dog
+ */
 typedef struct dog
 {
 	char *name;

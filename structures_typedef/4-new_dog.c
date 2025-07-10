0328726949c0cca77e9d1362167c48fc6b2625c3
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-10 15:00:34 +0200

    task 4 proj struct/typedef, simplier code, no strglen, no strgcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,45 +12,30 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int name_len, owner_len, i, j, n;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	i = 0;
-	while (name[i] != 0)
-		i++;
-	name_len = i;
-	new_dog->name = malloc(name_len + 1);
+	}
+
+	new_dog->name = malloc(sizeof(name));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	i = 0;
-	n = name_len;
-	while (i < n)
-	{
-		new_dog->name[i] = name[i];
-		i++;
-	}
-	j = 0;
-	while (name[j] != 0)
-		j++;
-	owner_len = j;
-	new_dog->owner = malloc(owner_len + 1);
+	new_dog->name = name;
+
+	new_dog->owner = malloc(sizeof(owner));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	n = owner_len;
-	j = 0;
-	while (j < n)
-	{
-		new_dog->owner[j] = owner[j];
-		j++;
-	}
+
 	new_dog->age = age;
+
 	return (new_dog);
 }

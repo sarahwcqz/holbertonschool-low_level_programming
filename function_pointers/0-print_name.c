599d3b1d5783fce3593714e8cd47297ac9d57757
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-15 09:59:54 +0200

    modif on script task 0 proj function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function that prints name
+ * @name: the name to print
+ * @(*f)(char *): the function that prints the name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}

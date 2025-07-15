513fc096c0902a2ccb753ff34fe531fbb7ab8f78
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-15 10:31:20 +0200

    modif betty task 0 and 1

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 /**
  * print_name - function that prints name
  * @name: the name to print
- * @(*f)(char *): the function that prints the name
+ * @f: the function that prints the name
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+f(name);
 }
--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,18 +1,18 @@
 #include "function_pointers.h"
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - exe a fctn given as a parameter on each elemnt of arr
  * @array: the array
  * @size: size of the array
- * @(*action)(int)
+ * @action: the function to exe
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int i = 0;
+unsigned int i = 0;
 
-    while (i < size)
+	while (i < size)
     {
-        action(array[i]);
-        i++;
-    }
+		action(array[i]);
+		i++;
+	}
 }

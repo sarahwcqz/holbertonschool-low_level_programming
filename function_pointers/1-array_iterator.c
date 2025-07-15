f839f01126a1be7f4672b702ff0892d689527d15
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-15 11:05:49 +0200

    task 2 proj function pointers, w/ task 1 betty modified

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,17 +1,17 @@
 #include "function_pointers.h"
 /**
- * array_iterator - exe a fctn given as a parameter on each elemnt of arr
- * @array: the array
- * @size: size of the array
- * @action: the function to exe
- * Return: void
- */
+* array_iterator - exe a fctn given as a parameter on each elemnt of arr
+* @array: the array
+* @size: size of the array
+* @action: the function to exe
+* Return: void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i = 0;
 
 	while (i < size)
-    {
+	{
 		action(array[i]);
 		i++;
 	}
---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an int
+ * @array: the array in which to look for
+ * @size: size of the array
+ * @cmp: pointer to the function to be used to compare value
+ * Return: first element found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || cmp == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+		return (-1);
+}

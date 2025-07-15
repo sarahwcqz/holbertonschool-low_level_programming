ca4e1efe5405ae5992c1bdebbc33b8701277a1af
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-15 10:23:28 +0200

    task 1 proj function pointer w/ header modified: adding stdio

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,11 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    int i = 0;
+    unsigned int i = 0;
 
     while (i < size)
     {
-        action(i);
+        action(array[i]);
         i++;
     }
 }

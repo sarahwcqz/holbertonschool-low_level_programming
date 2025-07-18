adf82f0babcebf633081db8e972ecfb35162d056
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-18 11:32:21 +0200

    modif on script 3 proj variadic functions, removing unnecessary flag

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,7 +4,7 @@
 
  /**
   * print_char - function that prints if it is a char
-  * @va_list args: the list of arguments that you passed to the fctn
+  * @args: the list of arguments that you passed to the fctn
   * Return: void
   */
 void print_char(va_list args)
@@ -14,7 +14,7 @@ void print_char(va_list args)
 
  /**
   * print_int - function that prints if it is a integer
-  * @va_list args: the list of arguments that you passed to the fctn
+  * @args: the list of arguments that you passed to the fctn
   * Return: void
   */
 void print_int(va_list args)
@@ -24,24 +24,25 @@ void print_int(va_list args)
 
  /**
   * print_float - function that prints if it is a float
-  * @va_list args: the list of arguments that you passed to the fctn
+  * @args: the list of arguments that you passed to the fctn
   * Return: void
   */
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
+
  /**
   * print_string - function that prints if it is a str of char
-  * @va_list args: the list of arguments that you passed to the fctn
+  * @args: the list of arguments that you passed to the fctn
   * Return: void
   */
 void print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
 
-/*	if (str == NULL)
-		str = "(nil)"; */
+/*	if (str == NULL) */
+/*		str = "(nil)"; */
 	printf("%s", str);
 }
 
@@ -67,9 +68,6 @@ void print_string(va_list args)
 	};
 
 	int i = 0, j = 0;
-/* flag = nous permet de savoir si il y a match entre le char et un char */
-/* de la structure ==> permet le if a la fin*/
-	int flag = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -77,21 +75,19 @@ void print_string(va_list args)
 	while (format[i] != '\0' && format)
 	{
 		j = 0;
-		flag = 0;
  /*se deplace dans la struct*/
 		while ((types[j].indiq) != NULL)
 		{
-			/* si le char en arg0 match un des char de l struct */
+			/* si le char en arg0 match un des char de la struct */
 			if (format[i] == (*types[j].indiq))
 			{
-			flag = 1;
 				types[j].print(args);
+/* print the comma and space ONLY IF it's not the last arg to be printed */
+				if (format[i + 1] != '\0')
+					printf(", ");
 			}
 			j++;
 		}
-/* print the comma and space ONLY IF it's not the last arg to be printed */
-		if ((format[i + 1] != '\0') && (flag == 1))
-			printf(", ");
 		i++;
 	}
 	va_end(args);

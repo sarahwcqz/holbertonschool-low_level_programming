59505d9643b0ce0b51b38369c79f3e34d402f118
sarahwcqz <sarah.wacquiez@gmail.com>
2025-07-29 13:23:26 +0200

    adding task 2 proj doub. linked lists

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
+
+	if (*head != 0)
+		(*head)->prev = new_node;
+
 	*head = new_node;
 
 	return (new_node);
